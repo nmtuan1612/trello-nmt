d21c38149834041560cd2aaa387514d70999398f
Nguyễn Tuấn <nguyentuan@MacBook-Pro-cua-mac.local>
2022-04-22 00:09:15 +0700

    clean react achitecture

---
+++ src/components/Task/Task.js
@@ -0,0 +1,15 @@
+import './Task.scss'
+
+const Task = () => {
+  return (
+    <li className="task-item">
+      <img
+        src="https://scontent.fhan14-1.fna.fbcdn.net/v/t45.1600-4/277895101_6292993313882_1968459955091717650_n.jpg?stp=cp0_dst-jpg_p180x540_q75_spS444&_nc_cat=107&ccb=1-5&_nc_sid=67cdda&_nc_ohc=peTOX7IllF0AX-wc6ov&_nc_ht=scontent.fhan14-1.fna&oh=00_AT8QmQNCN3S_7LV7W1e915MiUFFWlQyTKB0-5le9ESLtpA&oe=62664F45"
+        alt="task img"
+      />
+      Title: NMTuan
+    </li>
+  );
+};
+
+export default Task;
